12ce62bb92348810239a97aa88836d9ac199058e
Tony Ward <tonycward@gmail.com>
2018-03-03 16:50:07 +0000

    Release version 1

---
+++ BirminghamArtificialNeuralNet/Form1.Designer.vb
@@ -0,0 +1,334 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class BhamANN
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Column5 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column3 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column4 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column6 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.DataGridView2 = New System.Windows.Forms.DataGridView()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.DataGridView3 = New System.Windows.Forms.DataGridView()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox4 = New System.Windows.Forms.GroupBox()
+        Me.DataGridView4 = New System.Windows.Forms.DataGridView()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Column7 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column8 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column13 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column11 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column12 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column15 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column9 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column10 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column14 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DataGridView2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        CType(Me.DataGridView3, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox4.SuspendLayout()
+        CType(Me.DataGridView4, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(6, 27)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(102, 23)
+        Me.Button5.TabIndex = 4
+        Me.Button5.Text = "Run the network"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(18, 19)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(135, 23)
+        Me.Button3.TabIndex = 6
+        Me.Button3.Text = "Load OR (Training Data)"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(18, 19)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(136, 23)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "Load XOR (Target Data)"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Column5, Me.Column1, Me.Column2, Me.Column3, Me.Column4, Me.Column6})
+        Me.DataGridView1.Location = New System.Drawing.Point(6, 56)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(800, 256)
+        Me.DataGridView1.TabIndex = 8
+        '
+        'Column5
+        '
+        Me.Column5.HeaderText = "Epoch"
+        Me.Column5.Name = "Column5"
+        Me.Column5.Width = 125
+        '
+        'Column1
+        '
+        Me.Column1.HeaderText = "Variable 1"
+        Me.Column1.Name = "Column1"
+        Me.Column1.Width = 125
+        '
+        'Column2
+        '
+        Me.Column2.HeaderText = "Variable 2"
+        Me.Column2.Name = "Column2"
+        Me.Column2.Width = 125
+        '
+        'Column3
+        '
+        Me.Column3.HeaderText = "Variable 3"
+        Me.Column3.Name = "Column3"
+        Me.Column3.Width = 125
+        '
+        'Column4
+        '
+        Me.Column4.HeaderText = "Variable 4"
+        Me.Column4.Name = "Column4"
+        Me.Column4.Width = 125
+        '
+        'Column6
+        '
+        Me.Column6.HeaderText = "Network Error"
+        Me.Column6.Name = "Column6"
+        Me.Column6.Width = 125
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(629, 299)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(128, 13)
+        Me.Label1.TabIndex = 9
+        Me.Label1.Text = "Output of Neural Network"
+        '
+        'DataGridView2
+        '
+        Me.DataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView2.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Column7, Me.Column8})
+        Me.DataGridView2.Location = New System.Drawing.Point(6, 48)
+        Me.DataGridView2.Name = "DataGridView2"
+        Me.DataGridView2.Size = New System.Drawing.Size(247, 150)
+        Me.DataGridView2.TabIndex = 10
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.BackColor = System.Drawing.SystemColors.Control
+        Me.GroupBox2.Controls.Add(Me.DataGridView2)
+        Me.GroupBox2.Controls.Add(Me.Button1)
+        Me.GroupBox2.Location = New System.Drawing.Point(492, 25)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(269, 214)
+        Me.GroupBox2.TabIndex = 11
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "A XOR B, we should have this.."
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.BackColor = System.Drawing.SystemColors.Control
+        Me.GroupBox3.Controls.Add(Me.DataGridView3)
+        Me.GroupBox3.Controls.Add(Me.Button3)
+        Me.GroupBox3.Location = New System.Drawing.Point(12, 25)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(474, 214)
+        Me.GroupBox3.TabIndex = 12
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "A OR B, then we should have this..."
+        '
+        'DataGridView3
+        '
+        Me.DataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView3.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Column15, Me.Column9, Me.Column10, Me.Column14})
+        Me.DataGridView3.Location = New System.Drawing.Point(18, 48)
+        Me.DataGridView3.Name = "DataGridView3"
+        Me.DataGridView3.Size = New System.Drawing.Size(445, 150)
+        Me.DataGridView3.TabIndex = 0
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.DataGridView1)
+        Me.GroupBox1.Controls.Add(Me.Button5)
+        Me.GroupBox1.Location = New System.Drawing.Point(204, 258)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(838, 334)
+        Me.GroupBox1.TabIndex = 13
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "This is a feed forward - back propergation ANN using Sigmoid function... "
+        '
+        'GroupBox4
+        '
+        Me.GroupBox4.BackColor = System.Drawing.SystemColors.Control
+        Me.GroupBox4.Controls.Add(Me.Button2)
+        Me.GroupBox4.Controls.Add(Me.DataGridView4)
+        Me.GroupBox4.Location = New System.Drawing.Point(767, 25)
+        Me.GroupBox4.Name = "GroupBox4"
+        Me.GroupBox4.Size = New System.Drawing.Size(382, 214)
+        Me.GroupBox4.TabIndex = 14
+        Me.GroupBox4.TabStop = False
+        Me.GroupBox4.Text = "Show the results of the network"
+        '
+        'DataGridView4
+        '
+        Me.DataGridView4.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView4.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Column13, Me.Column11, Me.Column12})
+        Me.DataGridView4.Location = New System.Drawing.Point(16, 48)
+        Me.DataGridView4.Name = "DataGridView4"
+        Me.DataGridView4.Size = New System.Drawing.Size(344, 150)
+        Me.DataGridView4.TabIndex = 0
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(16, 19)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(89, 23)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Network Result"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Column7
+        '
+        Me.Column7.HeaderText = "Variable No."
+        Me.Column7.Name = "Column7"
+        Me.Column7.ToolTipText = "Results for an XOR variable A"
+        '
+        'Column8
+        '
+        Me.Column8.HeaderText = "XOR Result"
+        Me.Column8.Name = "Column8"
+        Me.Column8.ToolTipText = "Results for an XOR variable A"
+        '
+        'Column13
+        '
+        Me.Column13.HeaderText = "Variable No."
+        Me.Column13.Name = "Column13"
+        '
+        'Column11
+        '
+        Me.Column11.HeaderText = "Float Result"
+        Me.Column11.Name = "Column11"
+        '
+        'Column12
+        '
+        Me.Column12.HeaderText = "Round Result"
+        Me.Column12.Name = "Column12"
+        '
+        'Column15
+        '
+        Me.Column15.HeaderText = "Variable No."
+        Me.Column15.Name = "Column15"
+        '
+        'Column9
+        '
+        Me.Column9.HeaderText = "A"
+        Me.Column9.Name = "Column9"
+        Me.Column9.ToolTipText = "This is variable A"
+        '
+        'Column10
+        '
+        Me.Column10.HeaderText = "B"
+        Me.Column10.Name = "Column10"
+        Me.Column10.ToolTipText = "This is variable B"
+        '
+        'Column14
+        '
+        Me.Column14.HeaderText = "A Or B"
+        Me.Column14.Name = "Column14"
+        '
+        'BhamANN
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1285, 696)
+        Me.Controls.Add(Me.GroupBox4)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "BhamANN"
+        Me.Text = "Birmingham Artificial Neural Network (ANN)"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DataGridView2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox3.ResumeLayout(False)
+        CType(Me.DataGridView3, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox4.ResumeLayout(False)
+        CType(Me.DataGridView4, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Column5 As DataGridViewTextBoxColumn
+    Friend WithEvents Column1 As DataGridViewTextBoxColumn
+    Friend WithEvents Column2 As DataGridViewTextBoxColumn
+    Friend WithEvents Column3 As DataGridViewTextBoxColumn
+    Friend WithEvents Column4 As DataGridViewTextBoxColumn
+    Friend WithEvents Column6 As DataGridViewTextBoxColumn
+    Friend WithEvents DataGridView2 As DataGridView
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents DataGridView3 As DataGridView
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents GroupBox4 As GroupBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents DataGridView4 As DataGridView
+    Friend WithEvents Column7 As DataGridViewTextBoxColumn
+    Friend WithEvents Column8 As DataGridViewTextBoxColumn
+    Friend WithEvents Column15 As DataGridViewTextBoxColumn
+    Friend WithEvents Column9 As DataGridViewTextBoxColumn
+    Friend WithEvents Column10 As DataGridViewTextBoxColumn
+    Friend WithEvents Column14 As DataGridViewTextBoxColumn
+    Friend WithEvents Column13 As DataGridViewTextBoxColumn
+    Friend WithEvents Column11 As DataGridViewTextBoxColumn
+    Friend WithEvents Column12 As DataGridViewTextBoxColumn
+End Class
---
+++ BirminghamArtificialNeuralNet/Form1.vb
@@ -0,0 +1,279 @@
+﻿Imports System.IO
+Imports System.Math
+
+Public Class BhamANN
+    Public Const NUMIN As Int16 = 2
+    Public Const NUMHID As Int16 = 2
+    Public Const smallwt As Double = 0.5
+    Public Const NUMOUT As Int16 = 1
+    Public Const NUMPAT As Int16 = 4
+    Public Const SUCCESS As Double = 0.0002
+    Public Const MAX_EPOCH As Int32 = 2000000
+
+
+
+    Public DeltaWeightIH(NUMIN + 1, NUMHID + 1) As Double
+    Public WeightIH(NUMIN + 1, NUMHID + 1) As Double
+    Public DeltaWeightHO(NUMHID + 1, NUMOUT + 1) As Double
+    Public WeightHO(NUMHID + 1, NUMOUT + 1) As Double
+    Public Output(NUMPAT + 1, NUMOUT + 1) As Double
+    Public TrainingValues(4, 3) As Double
+    Public TargetValues(4, 3) As Double
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs)
+        TargetValues = LoadDataArray(TargetValues)
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs)
+        Dim i, j As Int16
+        Dim TheData As String
+        TheData = ""
+
+        For i = 0 To TargetValues.GetUpperBound(0) - 1
+            For j = 0 To TargetValues.GetUpperBound(1) - 1
+                TheData = TheData + CStr(TargetValues(i, j))
+            Next
+            MsgBox(i & " " & "Data is >" & " " & TheData)
+            TheData = ""
+        Next
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs)
+        TrainingValues = LoadDataArray(TrainingValues)
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs)
+        Dim i, j As Int16
+        Dim TheData As String
+        TheData = ""
+
+        For i = 0 To TrainingValues.GetUpperBound(0) - 1
+            For j = 0 To TrainingValues.GetUpperBound(1) - 1
+                TheData = TheData + CStr(TrainingValues(i, j))
+            Next
+            MsgBox(i & " " & "Data is >" & " " & TheData)
+            TheData = ""
+        Next
+    End Sub
+
+    Private Function LoadDataArray(ByVal ArrayToLoad(,) As Double) As Double(,)
+        Dim FileNameNeeded As String
+        FileNameNeeded = ""
+        If (OpenFileDialog1.ShowDialog = Windows.Forms.DialogResult.OK) Then
+            FileNameNeeded = OpenFileDialog1.FileName
+        End If
+        Dim reader As New StreamReader(FileNameNeeded)
+        Dim TheNextLine As String
+        Dim i, j As Integer
+        i = 0
+        j = 0
+        Do
+            TheNextLine = reader.ReadLine
+            If TheNextLine Is Nothing Then
+                Exit Do
+            End If
+            Dim Elements As String() = TheNextLine.Split(",")
+            j = 0
+            For Each Element In Elements
+                ArrayToLoad(i, j) = CDbl(Element)
+                j = j + 1
+            Next
+            i = i + 1
+        Loop Until TheNextLine Is Nothing
+        reader.Close()
+        Return ArrayToLoad
+        'MsgBox("Target set loaded")
+    End Function
+
+    Private Sub InitialiseWeightIH()
+        Dim i, j As Int16
+        For j = 1 To NUMHID
+            For i = 0 To NUMIN
+                DeltaWeightIH(i, j) = 0.0
+                WeightIH(i, j) = Abs(2.0 * (Rando() - 0.5) * smallwt)
+            Next
+        Next
+    End Sub
+
+    Private Function Rando() As Double
+        Dim NewRandom As New Random
+        'Dim Number As Double = NewRandom.Next(0, 1) + NewRandom.NextDouble()
+        Dim Number As Double = Rnd()
+        Return Abs(Number)
+    End Function
+
+    Private Sub InitialiseWeightHO()
+        Dim j, k As Int16
+        For k = 1 To NUMOUT
+            For j = 0 To NUMHID
+                DeltaWeightHO(j, k) = 0.0
+                WeightHO(j, k) = Abs(2.0 * (Rando() - 0.5) * smallwt)
+            Next
+        Next
+    End Sub
+
+    Private Sub BhamANN_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        InitialiseWeightIH()
+        InitialiseWeightHO()
+    End Sub
+
+    Private Function sigmoid(ByVal x As Double) As Double
+        Dim exp_value, return_value As Double
+        exp_value = Exp(-x)
+        return_value = 1 / (1 + exp_value)
+        Return return_value
+    End Function
+
+    Private Sub DriveTheNeuralNetwork()
+        Dim i, j, k, p, np, op, ranpat(NUMPAT + 1) As Int64
+        Dim SumH(NUMPAT + 1, NUMHID + 1), Hidden(NUMPAT + 1, NUMHID + 1), SumO(NUMPAT + 1, NUMOUT + 1) As Double
+        Dim DeltaO(NUMOUT + 1), SumDOW(NUMHID + 1), DeltaH(NUMHID + 1), Errored, epoch As Double
+        Dim eta As Double = 0.9
+        Dim alpha As Double = 0.5
+        Dim x, y As Int32
+        x = 1
+        y = 0
+        ''Ensure that the required number of rows are available
+        'If DataGridView1.Rows.Count < Output.GetUpperBound(0) Then
+        '    DataGridView1.Rows.Add((Output.GetUpperBound(0) + 1) - DataGridView1.Rows.Count)
+        'End If
+
+        For epoch = 0 To MAX_EPOCH
+            'MsgBox("Current epoch = " + CStr(epoch))
+            For p = 1 To NUMPAT ' randomize order of training patterns
+                ranpat(p) = p
+            Next
+            For p = 1 To NUMPAT
+                np = p + Rando() * (NUMPAT + 1 - p)
+                op = ranpat(p)
+                ranpat(p) = ranpat(np)
+                ranpat(np) = op
+            Next
+
+            Errored = 0.0
+            For np = 1 To NUMPAT
+                p = ranpat(np)
+                For j = 1 To NUMHID ' compute hidden unit activations */
+                    SumH(p, j) = WeightIH(0, j)
+                    For i = 1 To NUMIN
+                        SumH(p, j) += TrainingValues(p, i) * WeightIH(i, j)
+                        'SumH(p, j) += Input(p, i) * WeightIH(i, j)
+                    Next
+                    Hidden(p, j) = sigmoid(SumH(p, j))      '1.0/(1.0 + exp(-SumH[p][j])) ;*/
+                Next
+            Next
+            For k = 1 To NUMOUT   ' compute output unit activations And errors */
+                SumO(p, k) = WeightHO(0, k)
+                For j = 1 To NUMHID
+                    SumO(p, k) += Hidden(p, j) * WeightHO(j, k)
+                Next
+                Output(p, k) = sigmoid(SumO(p, k))    '1.0/(1.0 + exp(-SumO[p][k])) ;  Sigmoidal Outputs */
+                'Output[p][k] = SumO[p][k];      Linear Outputs */
+                Errored += 0.5 * (TargetValues(p, k) - Output(p, k)) * (TargetValues(p, k) - Output(p, k))   ' SSE */
+                'Error -= ( Target[p][k] * log( Output[p][k] ) + ( 1.0 - Target[p][k] ) * log( 1.0 - Output[p][k] ) ) ;    Cross-Entropy Error */
+                DeltaO(k) = (TargetValues(p, k) - Output(p, k)) * Output(p, k) * (1.0 - Output(p, k))   ' Sigmoidal Outputs, SSE */
+                'DeltaO[k] = Target[p][k] - Output[p][k];     Sigmoidal Outputs, Cross-Entropy Error */
+                'DeltaO[k] = Target[p][k] - Output[p][k];     Linear Outputs, SSE */
+            Next
+            For j = 1 To NUMHID     'back-propagate' errors to hidden layer */
+                SumDOW(j) = 0.0
+                For k = 1 To NUMOUT
+                    SumDOW(j) += WeightHO(j, k) * DeltaO(k)
+                Next
+                DeltaH(j) = SumDOW(j) * Hidden(p, j) * (1.0 - Hidden(p, j))
+            Next
+            For j = 1 To NUMHID  'update weights WeightIH */
+                DeltaWeightIH(0, j) = eta * DeltaH(j) + alpha * DeltaWeightIH(0, j)
+                WeightIH(0, j) += DeltaWeightIH(0, j)
+                For i = 1 To NUMIN
+                    DeltaWeightIH(i, j) = eta * TrainingValues(p, i) * DeltaH(j) + alpha * DeltaWeightIH(i, j)
+                    WeightIH(i, j) += DeltaWeightIH(i, j)
+                Next
+            Next
+            For k = 1 To NUMOUT 'update weights WeightHO */
+                DeltaWeightHO(0, k) = eta * DeltaO(k) + alpha * DeltaWeightHO(0, k)
+                WeightHO(0, k) += DeltaWeightHO(0, k)
+                For j = 1 To NUMHID
+                    DeltaWeightHO(j, k) = eta * Hidden(p, j) * DeltaO(k) + alpha * DeltaWeightHO(j, k)
+                    WeightHO(j, k) += DeltaWeightHO(j, k)
+                Next
+            Next
+            If epoch Mod 250 = 0 Then
+                DataGridView1.Rows.Add(New String() {epoch, Output(1, 1), Output(2, 1), Output(3, 1), Output(4, 1), Errored})
+            End If
+
+            If Errored < SUCCESS Then
+                DataGridView1.Rows.Add(New String() {epoch, Output(1, 1), Output(2, 1), Output(3, 1), Output(4, 1), Errored})
+                Exit For  'stop learning when 'near enough' */
+            End If
+        Next
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        DriveTheNeuralNetwork()
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs)
+        ShowArray.Show()
+    End Sub
+
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim i As Int16
+        DataGridView2.Columns(0).DefaultCellStyle.BackColor = Color.DarkGray
+        TargetValues = LoadDataArray(TargetValues)
+        For i = 1 To 4
+            DataGridView2.Rows.Add(New String() {i, TargetValues(i, 1)})
+        Next
+
+    End Sub
+
+    Private Sub Button3_Click_1(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim i As Int16
+        Dim OrValue As Int16 = 0
+
+        TrainingValues = LoadDataArray(TrainingValues)
+        DataGridView3.Columns(0).DefaultCellStyle.BackColor = Color.DarkGray
+        For i = 1 To 4
+            If TrainingValues(i, 1) Or TrainingValues(i, 2) = 1 Then
+                OrValue = 1
+            Else
+                OrValue = 0
+            End If
+            DataGridView3.Rows.Add(New String() {i, TrainingValues(i, 1), TrainingValues(i, 2), OrValue})
+        Next
+
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) 
+        Dim tony As Int16
+        Dim ward As Int16
+        Dim TheResult As String = ""
+        For tony = 1 To 4
+            For ward = 1 To 2
+                TheResult = TheResult + CStr(Round(Output(tony, ward)))
+            Next
+            TheResult = TheResult + vbCrLf
+        Next
+        MsgBox(TheResult)
+    End Sub
+
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
+
+    End Sub
+
+    Private Sub Button2_Click_2(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim i As Int16
+        DataGridView4.Columns(0).DefaultCellStyle.BackColor = Color.DarkGray
+        For i = 1 To 4
+            DataGridView4.Rows.Add(New String() {i, Output(i, 1), Round(Output(i, 1))})
+        Next
+    End Sub
+
+    Private Sub GroupBox2_Enter(sender As Object, e As EventArgs) Handles GroupBox2.Enter
+
+    End Sub
+
+    Private Sub GroupBox3_Enter(sender As Object, e As EventArgs) Handles GroupBox3.Enter
+
+    End Sub
+End Class
